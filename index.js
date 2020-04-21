8d8940b39bbdcf21ac8f624106f52e4cecb28cb3
Mihir Chaturvedi <plibither8@gmail.com>
2020-04-21 20:25:46 +0530

    Init: Add initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const {Octokit} = require('@octokit/rest');
+const fetch = require('node-fetch');
+
+// GitHub Gists env variables
+const {
+	GIST_ID,
+	GITHUB_TOKEN
+} = process.env;
