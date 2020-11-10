ebcad97aacfe939801b5c2c9a3a88b23f6bd649c
Mihir Chaturvedi <plibither8@gmail.com>
2020-11-10 22:27:42 +0530

    Fix: Massive changes to make it work on DO

--- index.js
+++ index.js
@@ -1,91 +1,116 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const {Octokit} = require('@octokit/rest');
-const fetch = require('node-fetch');
+const { Octokit } = require('@octokit/rest')
+const fetch = require('node-fetch')
 
 // GitHub Gists env variables
 const {
-	GIST_ID,
-	GH_TOKEN,
-	TG_BOT_NAME,
-	TG_BOT_SECRET
-} = process.env;
+  GIST_ID,
+  GH_TOKEN,
+  TG_BOT_NAME,
+  TG_BOT_SECRET
+} = process.env
 
 const octokit = new Octokit({ auth: `token ${GH_TOKEN}` }) // Instantiate Octokit
 
-const getOldData = async () => {
-	const gist = await octokit.gists.get({ gist_id: GIST_ID });
-	return JSON.parse(gist.data.files['gh-followers.json'].content);
-};
+async function getOldData () {
+  const gist = await octokit.gists.get({ gist_id: GIST_ID })
+  return JSON.parse(gist.data.files['gh-followers.json'].content)
+}
 
-const getNewData = async () => {
-	let newData = [];
-	let page = 1;
-	while (true) {
-		const list = (
-				await fetch('https://api.github.com/users/plibither8/followers?per_page=100&page='+(page++))
-				.then(res => res.json())
-			).map(fol => fol.login);
+async function getNewData () {
+  let newData = []
+  let page = 1
 
-		if (list.length == 0) {
-			break;
-		}
+  while (true) {
+    const list = (
+      await fetch('https://api.github.com/users/plibither8/followers?per_page=100&page=' + (page++))
+        .then(res => res.json())
+    ).map(fol => fol.login)
 
-		newData = newData.concat(list);
-	}
-	return newData;
-};
+    if (!Number(list.length)) {
+      break
+    }
 
-const compareData = (oldData, newData) => ({
-	removed: oldData.filter(fol => newData.indexOf(fol) === -1),
-	added: newData.filter(fol => oldData.indexOf(fol) === -1)
-});
+    newData = newData.concat(list)
+  }
 
-const notify = async (changes, followerCount) => {
-	let message = '*🐙 GitHub followers list updated!*';
-	message += `\nNumber of followers: ${followerCount}`;
+  return newData
+}
 
-	if (changes.removed.length > 0) {
-		message += '\n\nUnfollowed:\n';
-		message += changes.removed.map(fol => `- [${fol}](https://github.com/${fol})`).join('\n');
-	}
-	if (changes.added.length > 0) {
-		message += '\n\nFollowed:\n';
-		message += changes.added.map(fol => `- [${fol}](https://github.com/${fol})`).join('\n');
-	}
+function compareData (oldData, newData) {
+  return {
+    removed: oldData.filter(fol => newData.indexOf(fol) === -1),
+    added: newData.filter(fol => oldData.indexOf(fol) === -1)
+  }
+}
 
-	await fetch(`https://tg.mihir.ch/${TG_BOT_NAME}`, {
-		method: 'POST',
-		body: JSON.stringify({
-			text: message,
-			secret: TG_BOT_SECRET
-		}),
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-};
+async function notify (changes, followerCount) {
+  let message = '*🐙 GitHub followers list updated!*'
+  message += `\nNumber of followers: ${followerCount}`
 
-const putNewData = async newData => {
-	await octokit.gists.update({
-		gist_id: GIST_ID,
-		files: {
-			'gh-followers.json': {
-				content: JSON.stringify(newData, null, '  ')
-			}
-		}
-	});
-};
+  if (changes.removed.length > 0) {
+    message += '\n\nUnfollowed:\n'
+    message += changes.removed.map(fol => `- [${fol}](https://github.com/${fol})`).join('\n')
+  }
+  if (changes.added.length > 0) {
+    message += '\n\nFollowed:\n'
+    message += changes.added.map(fol => `- [${fol}](https://github.com/${fol})`).join('\n')
+  }
 
-const main = async () => {
-	const oldData = await getOldData();
-	const newData = await getNewData();
+  await fetch(`https://tg.mihir.ch/${TG_BOT_NAME}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      text: message,
+      secret: TG_BOT_SECRET
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
 
-	const changes = compareData(oldData, newData);
-	if (changes.removed.length + changes.added.length === 0) return;
+async function putNewData (newData) {
+  await octokit.gists.update({
+    gist_id: GIST_ID,
+    files: {
+      'gh-followers.json': {
+        content: JSON.stringify(newData, null, '  ')
+      }
+    }
+  })
+}
 
-	await notify(changes, newData.length);
-	await putNewData(newData);
-};
+async function checkAndUpdate (oldData) {
+  console.log('Fetching new data from GitHub...')
+  const newData = await getNewData()
+  console.log('New data fetched from GitHub')
 
-(async () => await main())();
+  const changes = compareData(oldData, newData)
+  const delta = changes.removed.length + changes.added.length
+
+  console.log('delta:', delta)
+
+  if (delta > 0) {
+    console.log('Data changed, notifying and updating to gist...')
+    await notify(changes, newData.length)
+    await putNewData(newData)
+  } else {
+    console.log('Data unchanged')
+  }
+
+  return newData
+}
+
+async function main () {
+  console.log('Fetching old data from gist...')
+  let oldData = await getOldData()
+  console.log('Old data fetched from gist')
+
+  oldData = await checkAndUpdate(oldData)
+  setInterval(async () => {
+    oldData = await checkAndUpdate(oldData)
+  }, 1200000) // 20 minutes
+}
+
+main()
